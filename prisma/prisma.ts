4745d63d40172efbaa24b49a70b9e3540addd4ba
1Dambek1 <denolinevichd@inbox.ru>
2025-03-25 04:13:28 +0800

    refactor prisma setup

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -4,6 +4,12 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma || new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.POSTGRES_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
